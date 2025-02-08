9edba8d8b4c5fdab8483e581c793e21efb2dd483
diddiakhil <akhil.diddi@innoitlabs.com>
2025-02-08 10:04:38 +0530

    in settings image upload is pending chaned protecter router if token expired it will logout and it navigate to login page

--- src/Components/Account/Account.jsx
+++ src/Components/Account/Account.jsx
@@ -224,7 +224,9 @@ const Account = () => {
                     label={"Password"}
                     name="password"
                     value={profile.password}
-                    onChange={handleChange}
+                    onChange={(e) =>
+                      setProfile((prev) => ({ ...prev, password: e.target.value }))
+                    }
                     placeholder={"Password"}
                   />
                 </div>
@@ -233,7 +235,9 @@ const Account = () => {
                     label={"Confirm Password"}
                     name="confirmPassword"
                     value={profile.confirmPassword}
-                    onChange={handleChange}
+                    onChange={(e) =>
+                      setProfile((prev) => ({ ...prev, confirmPassword: e.target.value }))
+                    }
                     placeholder={"Confirm Password"}
                   />
                 </div>
