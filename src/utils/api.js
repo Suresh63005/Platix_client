ea1c4166290b19d6455e976b85e37d9e46ea378c
diddiakhil <akhil.diddi@innoitlabs.com>
2025-02-13 18:42:26 +0530

    cjahewvavcmc

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,7 +2,8 @@ import axios from "axios";
 import Cookies from "js-cookie"; 
 
 const api = axios.create({
-  baseURL: "http://localhost:5000" || "http://localhost:8081",
+  // baseURL: "http://localhost:5000" || "http://localhost:8081",
+  baseURL: "https://platix-server.vercel.app/",
   withCredentials: true, 
 });
 
